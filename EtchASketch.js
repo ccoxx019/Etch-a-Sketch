e3427e5d09afb599ade59aba0ea221aff26253d8
Craig Cox <ccoxx019@gmail.com>
2020-08-19 11:37:47 -0400

    working now

--- EtchASketch.js
+++ EtchASketch.js
@@ -1,2 +1,19 @@
 alert("conn");
 
+var board = document.querySelector("#board");
+var boxes = document.querySelectorAll(".box");
+var numberOfBoxes = 10000;
+
+function createGrid(cells) {
+    for (let i = 0; i < cells; i++) {
+        let div = document.createElement("div");
+        div.classList.add("box");
+        board.appendChild(div);
+        div.addEventListener("mouseover", function() {
+            div.classList.add("active");
+        })
+    };
+};
+
+
+createGrid(numberOfBoxes);
