4365c3f40f29e2de86db2e4302de93353d5df769
Craig Cox <ccoxx019@gmail.com>
2020-08-19 12:45:21 -0400

    reset button added

--- EtchASketch.js
+++ EtchASketch.js
@@ -1,17 +1,24 @@
 var board = document.querySelector("#board");
-var boxes = document.querySelectorAll(".box");
+var boxes = document.querySelectorAll(".cell");
 var numberOfBoxes = 10000;
 
 function createGrid(cells) {
     for (let i = 0; i < cells; i++) {
         let div = document.createElement("div");
-        div.classList.add("box");
+        div.classList.add("cell");
         board.appendChild(div);
+        //color cell when cursor moves over
         div.addEventListener("mouseover", function() {
             div.classList.add("active");
         })
     };
 };
 
+function resetCells() {
+    var cells = document.querySelectorAll(".cell");
+    [].forEach.call(cells, function(el) {
+        el.classList.remove("active");
+});
+}
 
 createGrid(numberOfBoxes);
